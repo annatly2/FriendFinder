47ba2f5ad2f3676fc9772788438b4d128f695994
Anna Ly <annatly2@gmail.com>
2017-11-03 19:33:13 -0700

    nov 3

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,18 +3,35 @@ var friendsArray = [
     name: "Scott",
     photo: "https://static.boredpanda.com/blog/wp-content/uploads/2017/03/cute-smiling-happy-quokkas-21-58c8fc60c32d4__605.jpg",
     scores: [
-      5,
-      4,
-      5,
-      4,
-      1,
-      2,
-      4,
-      5,
-      3,
-      2
+      "5",
+      "4",
+      "5",
+      "4",
+      "1",
+      "2",
+      "4",
+      "5",
+      "3",
+      "2"
+    ]
+  },{
+    name: "Matilda",
+    photo: "https://i.pinimg.com/736x/35/e8/40/35e8407cca3a26188e78b75847b2d883--teddy-bears-fluffy-bunny.jpg",
+    scores: [
+      "3",
+      "4",
+      "5",
+      "4",
+      "1",
+      "1",
+      "4",
+      "5",
+      "3",
+      "4"
     ]
   }
+
+
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,8 @@
 var friendsData = require("../data/friends.js");
+var path = require("path");
+
+var idealDifference = 0;
+
 
 module.exports = function(app){
 
@@ -9,11 +13,32 @@ module.exports = function(app){
 
 
   app.post("/api/friends", function(req, res){
-
+    var allFriendDifferences = [];
+    //loops through each friend
     for(i = 0; i < friendsData.length; i++){
-      friendsData.push(req.body);
-      res.json(true);
+      var friendDifference = 0;
+      //loops through each score for given friend
+      for(j = 0; j < friendsData[i].scores; j++){
+        friendDifference += Math.abs(friendsData[i].scores[j] - req.body.scores[j]);
+      }
+      allFriendDifferences.push(friendDifference);
+
     }
+    
+    function indexOfSmallest(a) {
+      var lowest = 0;
+      for (var i = 1; i < a.length; i++) {
+       if (a[i] < a[lowest]) lowest = i;
+      }
+      return lowest;
+     }
+
+     var bestFriendIndex = indexOfSmallest(allFriendDifferences);
+     var bestFriend = friendsData[bestFriendIndex];
+
+      friendsData.push(req.body);
+      res.json(bestFriend);
+    
 
 
     });
